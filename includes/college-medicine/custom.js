269df6d1c58de295ce491f08f5972f7f099286ab
Jim Laney <jlaney@uw.edu>
2015-10-07 12:30:23 -0700

    SOM statics

--- includes/college-medicine/custom.js
+++ includes/college-medicine/custom.js
@@ -13,7 +13,7 @@
         /*EvaluationKIT START*/
         var evalkit_jshosted = document.createElement('script');
         evalkit_jshosted.setAttribute('type', 'text/javascript');
-        evalkit_jshosted.setAttribute('src', 'https://uwsom.evaluationkit.com/CanvasScripts/uwsom.js?v=5');
+        evalkit_jshosted.setAttribute('src', 'https://uwsom.evaluationkit.com/CanvasScripts/uwsom.js?v=6');
         document.getElementsByTagName('head')[0].appendChild(evalkit_jshosted);
         /*EvaluationKIT END*/
     }
