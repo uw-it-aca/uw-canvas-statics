bfd01586da2d3f9e5b7ff437a3bff75f670784df
mike seibel <mikes@washington.edu>
2016-02-19 13:41:06 -0800

    extends jqueyr with whenExists, refactors custom.js

--- includes/uw-global/custom.js
+++ includes/uw-global/custom.js
@@ -7,19 +7,37 @@
     $("head").append('<link rel="shortcut icon" type="image/x-icon" href="https://apps.canvas.uw.edu/branding/uw-global/favicon.ico"></link>');
 
     var catalog = {
-        add_people_label: 'Provide a list of valid (student, faculty, or staff) UW NetIDs:<br/>',
-        add_people_extra: '<div class="add_people_uwnetids">Note: Canvas recognizes only current student, faculty, and staff NetIDs by default.  To add other NetIDs or Gmail addresses, please read the <a href="http://www.washington.edu/itconnect/learn/tools/canvas/canvas-help-for-instructors/communicating/give-course-access-to-people-without-a-uw-netid/" target="_blank">UW course access guide</a> or contact <a href="mailto:help@uw.edu">help@uw.edu</a>.</div>',
-        add_people_error: '<div>Please enter a list of valid UW NetIDs or Gmail addresses</div>',
-        add_people_placeholder: 'uwnetid, uwnetid, uwnetid',
         public_syllabus_label: '<span class="aside checklabelfix"> (When publicly visible, students will be able to read the syllabus description text when registering for courses. <a href="http://www.washington.edu/itconnect/learn/tools/canvas/use-canvas-to-publicize-your-individual-course-description/" target="_blank">Learn more</a>.)</span>',
         app_center_info: 'Apps are an easy way to add new features to Canvas. They can be added to individual courses, or to all courses in an account. Once configured, you can link to them through course modules and create assignments for assessment tools.<br/><br/>UW-IT has reviewed the third-party apps listed below, and found they meet our established minimum criteria. <a href="http://www.washington.edu/itconnect/learn/tools/canvas/canvas-help-for-instructors/canvas-app-center/" target="_blank">Find more information</a> or get help with the App Center.',
         gpa_scale_info: 'Use GPA Scale Grading to create a UW 4.0 grading scale. To create your scale, in the left-hand course navigation, go to &quot;UW 4.0 Grade Scale&quot;. Once you create a scale, it will be added to this course and ready for you to apply to assignments. To learn more, see <a href="http://www.washington.edu/itconnect/learn/tools/canvas/canvas-help-for-instructors/assignments-grading/grading/4-0-grade-scale/" target="_blank">Use a 4.0 Grade Scale for Canvas Assignments</a>.'
     },
         app_center_attempts = 0,
+        /* BEGIN PROD */
         add_users_external_id = "31483",
         uw_groups_external_id = "31485",
         course_photos_external_id = "31493",
+        /* END PROD */
+        /* BEGIN TEST *
+        add_users_external_id = "30967",
+        uw_groups_external_id = "8655",
+        course_photos_external_id = "30978",
+        * END TEST */
         window_message;
+
+    $.fn.whenExists = function (handler) {
+        var selector = this.selector,
+            n = 0,
+            $nodes,
+            iid = window.setInterval(function () {
+                $nodes = $(selector);
+                $nodes.each(handler);
+                if ($nodes.length > 0 || ++n > 200) {
+                  window.clearInterval(iid);
+                }
+            }, 50);
+
+        return $(selector);
+    };
 
     function gettext(key) {
         return (catalog.hasOwnProperty(key)) ? catalog[key] : "";
@@ -27,27 +45,38 @@
 
     // Custom text for App Center
     function customizeAppCenterInfo() {
-        if ($("#external_tools").find("p:first").length) {
-            $("#external_tools").find("p:first").html(gettext("app_center_info"));
-        } else {
-            if (app_center_attempts < 50) { // Try for 5 secs
-                app_center_attempts++;
-                setTimeout(customizeAppCenterInfo, 100);
-            }
-        }
+        // Custom text for App Center
+        $("#external_tools").whenExists(function () {
+            $(this).find("p:first").html(gettext("app_center_info"));
+        });
     }
 
     // Custom text for "public syllabus" option
     function customizePublicSyllabusLabel() {
-        var text = gettext("public_syllabus_label");
-        $("label[for='course_public_syllabus']").after(text);
+        // Custom text for "public syllabus" option
+        $("label[for='course_public_syllabus']").whenExists(function () {
+            $(this).after(gettext("public_syllabus_label"));
+        });
     }
 
     // Custom text for grading scale modal
     function customizeGPAScaleDialog() {
-        setTimeout(function () {
-            $("#gpa-scale-dialog").html(gettext("gpa_scale_info"));
-        }, 50);
+        // Custom text for grading scale modal
+        $("#gpa-scale-dialog").whenExists(function () {
+            $(this).html(gettext("gpa_scale_info"));
+        });
+    }
+
+    function customizeGradebookMenu() {
+        // Remove the "Treat Ungraded as 0" menu item
+        $("#include_ungraded_assignments").whenExists(function () {
+            $(this).closest("li").remove();
+        });
+
+        // Remove the option on the Individual view, too
+        $("#ungraded").whenExists(function () {
+            $(this).closest("div").remove();
+        });
     }
 
     // Custom text for "Report a Problem" form
@@ -58,18 +87,11 @@
         }
     }
 
-    function customizeGradebookMenu() {
-        // Remove the "Treat Ungraded as 0" menu item
-        $("#include_ungraded_assignments").closest("li").remove();
-        // Remove the option on the Individual view, too
-        $("#ungraded").closest("div").remove();
-    }
-
     function hijackAddUsersButton() {
-      var $tool = $('.context_external_tool_' + add_users_external_id);
+        var $tool = $('.context_external_tool_' + add_users_external_id);
         if ($tool.length !== 1) {
-          return;
-      }
+            return;
+        }
 
         $("#addUsers")
             .off("click")
@@ -84,8 +106,7 @@
                       + 'src="' + $tool.attr('href') + '"'
                       + '></iframe><div id="uw_add_users"></div></div>').appendTo('body');
                 }
-            })
-            .css('display', 'inline');
+            });
     }
 
     function addCoursePhotosButton(tool_id) {
@@ -186,37 +207,37 @@
     }
 
     $(document).ready(function () {
-        setTimeout(function () {
-            var href = window.location.href,
-                tool_id;
+        var href = window.location.href,
+        tool_id;
 
-            if (href.match(/\/settings$/)) {
-                // Course or account settings page
-                customizePublicSyllabusLabel();
-                customizeAppCenterInfo();
-            } else if (href.match(/\/courses\/\d+\/assignments/)) {
-                // Course assignments page
-                $("#gpa-scale-question").on("click", customizeGPAScaleDialog);
-            } else if (href.match(/\/courses\/\d+\/users(\/(#.*)?)?$/)) {
-                // Course people page
-                addCoursePhotosButton(course_photos_external_id);
-                addUWGroupsButton(uw_groups_external_id);
-                hijackAddUsersButton();
-            } else if (href.match(/\/courses\/\d+\/gradebook/)) {
-                // Course gradebook page
-                customizeGradebookMenu();
-            } else {
-                tool_id = isSpecialToolPage([uw_groups_external_id,
-                                             course_photos_external_id]);
-                if (tool_id !== 0) {
-                    // UW Groups LTI or Course Photos LTI
-                    addPeopleBreadCrumbs(tool_id);
-                    addBackToPeopleButton();
-                }
+        if (href.match(/\/settings$/)) {
+            // Course or account settings page
+            customizePublicSyllabusLabel();
+            customizeAppCenterInfo();
+        } else if (href.match(/\/courses\/\d+\/assignments/)) {
+            // Course assignments page
+            $("#gpa-scale-question").whenExists(function () {
+                $(this).on("click", customizeGPAScaleDialog);
+            });
+        } else if (href.match(/\/courses\/\d+\/users(\/(#.*)?)?$/)) {
+            // Course people page
+            addCoursePhotosButton(course_photos_external_id);
+            addUWGroupsButton(uw_groups_external_id);
+            $('#addUsers').whenExists(hijackAddUsersButton);
+        } else if (href.match(/\/courses\/\d+\/gradebook/)) {
+            // Course gradebook page
+            customizeGradebookMenu();
+        } else {
+            tool_id = isSpecialToolPage([uw_groups_external_id,
+                                         course_photos_external_id]);
+            if (tool_id !== 0) {
+                // UW Groups LTI or Course Photos LTI
+                addPeopleBreadCrumbs(tool_id);
+                addBackToPeopleButton();
             }
+        }
 
-            $(document).on("ajaxComplete", customizeReportProblemForm);
-        }, 1000);
+        $(document).on("ajaxComplete", customizeReportProblemForm);
 
         $(window).on('message', function (e) {
             var msg = JSON.parse(e.originalEvent.data);
@@ -225,7 +246,7 @@
                 case 'lti.uw.addUserUrl':
                     if (window.top !== window) { // bubble up
                         window.parent.postMessage(e.originalEvent.data,
-                                                  'https://canvas.uw.edu');
+                                                  window.location.origin);
                     } else {
                         $.ajax({
                             type: 'GET',
