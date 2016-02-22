65227018d5250c08fb3ae8d1250734e21abbd258
mike seibel <mikes@washington.edu>
2016-02-22 15:11:49 -0800

    removes test tool ids

--- includes/uw-global/custom.js
+++ includes/uw-global/custom.js
@@ -12,16 +12,9 @@
         gpa_scale_info: 'Use GPA Scale Grading to create a UW 4.0 grading scale. To create your scale, in the left-hand course navigation, go to &quot;UW 4.0 Grade Scale&quot;. Once you create a scale, it will be added to this course and ready for you to apply to assignments. To learn more, see <a href="http://www.washington.edu/itconnect/learn/tools/canvas/canvas-help-for-instructors/assignments-grading/grading/4-0-grade-scale/" target="_blank">Use a 4.0 Grade Scale for Canvas Assignments</a>.'
     },
         app_center_attempts = 0,
-        /* BEGIN PROD */
         add_users_external_id = "31483",
         uw_groups_external_id = "31485",
         course_photos_external_id = "31493",
-        /* END PROD */
-        /* BEGIN TEST *
-        add_users_external_id = "30967",
-        uw_groups_external_id = "8655",
-        course_photos_external_id = "30978",
-        * END TEST */
         window_message;
 
     $.fn.whenExists = function (handler) {
