cc071d6e0ede7bd6e7db9d314ba6d3cfca9d4b99
Jim Laney <jlaney@uw.edu>
2017-07-27 10:44:19 -0700

    updates college-educ statics

--- includes/college-education/custom.js
+++ includes/college-education/custom.js
@@ -5,8 +5,21 @@
 
     // If Early EDU home page, then remove the right sidebar and expand the page
     $(".early-edu").whenExists(function () {
-        $(".with-right-side #right-side-wrapper").hide();
+        $(".ic-app-main-content")
+            .css({"flex": "", "-webkit-flex": "", "display": "block"});
+        $(".ic-Layout-contentWrapper")
+            .css({"flex": "", "-webkit-flex": "", "display": "block"});
         $("body:not(.full-width):not(.outcomes) .ic-Layout-wrapper")
             .css({"max-width": "none"});
+    });
+    $(".early-edu-discussion").whenExists(function () {
+        $(".discussion-reply-box").addClass("early-edu-discussion-button")
+            .children("span:first").text("Contribute to the Discussion");
+    });
+    $("#early-edu-accordion").whenExists(function () {
+        $("#early-edu-accordion").accordion({
+            collapsible: true,
+            heightStyle: "content"
+        });
     });
 }(jQuery));
