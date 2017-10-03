6946a48a1ead38e073e1a4b58c93b91277402b88
Jim Laney <jlaney@uw.edu>
2017-10-03 15:39:02 -0700

    updates front_page attribute for pages

--- includes/uw-global/pages.js
+++ includes/uw-global/pages.js
@@ -3,27 +3,74 @@
 (function ($) {
     'use strict';
 
-    function deselect_front_page() {
+    function deselect_front_page(ev) {
+        var page_url = ev.data.url,
+            url_regex = new RegExp('/courses/'),
+            csrf_regex = new RegExp('^_csrf_token=(.*)$'),
+            url = page_url.replace(url_regex, '/api/v1/courses/'),
+            cookies = document.cookie.split(';'),
+            cookie,
+            match,
+            csrf;
 
+        for (var i = 0; i < cookies.length; i++) {
+            cookie = cookies[i].trim();
+            match = csrf_regex.exec(cookie);
+            if (match) {
+                csrf = decodeURIComponent(match[1]);
+                break;
+            }
+        }
+
+        if (url && csrf) {
+            $.ajax({
+                type: 'PUT',
+                url: url,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': csrf
+                },
+                data: JSON.stringify({'wiki_page': {'front_page': false}}),
+                dataType: 'json',
+            }).done(function () {
+                window.location.href = window.location.href;
+            });
+        }
+        return false;
     }
 
     function add_deselect_front_page_option () {
         /*jshint validthis: true */
-        console.log("GOT A FRONT PAGE");
-        var previous_item = $(this).closest('tr').find('ul.al-options:eq(0) li:eq(1)'),
-            new_item = $('li').addClass('ui-menu-item').attr('role', 'presentation');
 
-        $('a', {
-            text: 'Deselect as Front Page',
-            title: 'Deselect as Front Page',
-            href: '#',
-            role: 'menuitem',
-            click: deselect_front_page
-        }).addClass('icon-home ui-corner-all').appendTo(new_item);
+        // This is the table of contents view
+        var item = $(this).closest('tr').find('a.delete-menu-item').first(),
+            url = $(this).closest('tr').find('a.wiki-page-link').first().attr('href'),
+            label = 'Deselect as Front Page';
 
-        new_item.insertAfter(previous_item);
+        if (item.length === 0) {
+            // This is the page edit view
+            item = $(this).closest('div').find('a.delete_page').first();
+            url = window.location.href;
+        }
+
+        item.replaceWith(
+            $('<a>', {
+                'text': label,
+                'title': label,
+                'href': '#',
+                'role': 'menuitem',
+                'class': 'icon-document ui-corner-all',
+            }).on('click', {'url': url}, deselect_front_page)
+        );
     }
 
-    // Remove front page option
+    // Add the remove front page option
     $('span.front-page').whenExists(add_deselect_front_page_option);
+
+    // ..and on the ajax request
+    $(document).ajaxComplete(function(ev, xhr, data) {
+        if (data.type === 'PUT' && data.url.match(/courses/)) {
+            $('span.front-page').whenExists(add_deselect_front_page_option);
+        }
+    });
 }(jQuery));
