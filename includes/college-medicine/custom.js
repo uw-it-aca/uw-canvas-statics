e88f48e7be6e656c9d285767fde0bd983bbdd89e
Jim Laney <jlaney@uw.edu>
2017-11-27 12:07:25 -0800

    updates version for evalkit statics

--- includes/college-medicine/custom.js
+++ includes/college-medicine/custom.js
@@ -1,9 +1,9 @@
 /*jslint browser: true, plusplus: true */
 /*global jQuery */
-(function ($) {
-    'use strict';
 
-    $(document).ready(function () {
-        $.getScript('https://uwsom.evaluationkit.com/CanvasScripts/uwsom.js?v=6');
-    });
-}(jQuery));
+/*EvaluationKIT START*/
+var evalkit_jshosted = document.createElement('script');
+evalkit_jshosted.setAttribute('type', 'text/javascript');
+evalkit_jshosted.setAttribute('src', 'https://uwsom.evaluationkit.com/CanvasScripts/uwsom.js?v=7');
+document.getElementsByTagName('head')[0].appendChild(evalkit_jshosted);
+/*EvaluationKIT END*/
