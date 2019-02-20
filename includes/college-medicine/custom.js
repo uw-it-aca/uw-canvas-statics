4d986b393151049c3cc239eb21257332cce37e51
Jim Laney <jlaney@uw.edu>
2019-02-20 13:37:31 -0800

    adds designtools to SoM statics

--- includes/college-medicine/custom.js
+++ includes/college-medicine/custom.js
@@ -7,3 +7,51 @@ evalkit_jshosted.setAttribute('type', 'text/javascript');
 evalkit_jshosted.setAttribute('src', 'https://uwsom.evaluationkit.com/CanvasScripts/uwsom.js?v=7');
 document.getElementsByTagName('head')[0].appendChild(evalkit_jshosted);
 /*EvaluationKIT END*/
+
+////////////////////////////////////////////////////
+// DESIGN TOOLS CONFIG                            //
+////////////////////////////////////////////////////
+// Copyright (C) 2017  Utah State University
+var DT_variables = {
+        iframeID: '',
+        // Path to the hosted USU Design Tools
+        path: 'https://designtools.ciditools.com/',
+        templateCourse: '1313636',
+        // OPTIONAL: Button will be hidden from view until launched using shortcut keys
+        hideButton: true,
+    	 // OPTIONAL: Limit by course format
+	     limitByFormat: false, // Change to true to limit by format
+	     // adjust the formats as needed. Format must be set for the course and in this array for tools to load
+	     formatArray: [
+            'online',
+            'on-campus',
+            'blended'
+        ],
+        // OPTIONAL: Limit tools loading by users role
+        limitByRole: false, // set to true to limit to roles in the roleArray
+        // adjust roles as needed
+        roleArray: [
+            'student',
+            'teacher',
+            'admin'
+        ],
+        // OPTIONAL: Limit tools to an array of Canvas user IDs
+        limitByUser: false, // Change to true to limit by user
+        // add users to array (Canvas user ID not SIS user ID)
+        userArray: [
+            '1234',
+            '987654'
+        ]
+};
+
+// Run the necessary code when a page loads
+$(document).ready(function () {
+    'use strict';
+    // This runs code that looks at each page and determines what controls to create
+    $.getScript(DT_variables.path + 'js/master_controls.js', function () {
+        console.log('master_controls.js loaded');
+    });
+});
+////////////////////////////////////////////////////
+// END DESIGN TOOLS CONFIG                        //
+////////////////////////////////////////////////////
