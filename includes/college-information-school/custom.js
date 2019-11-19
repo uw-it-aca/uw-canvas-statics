88117efed594b4b300f35355215bab171f040589
Jim Laney <jlaney@uw.edu>
2019-11-19 15:31:17 -0800

    add eesysoft integration

--- includes/college-information-school/custom.js
+++ includes/college-information-school/custom.js
@@ -1,35 +1,39 @@
 /*jslint browser: true, plusplus: true */
 /*global jQuery, UWCanvas */
 
-////////////////////////////////////////////////////
-// DESIGN TOOLS CONFIG                            //
-////////////////////////////////////////////////////
+// DESIGN TOOLS CONFIG
 // Copyright (C) 2016  Utah State University
 var DT_variables = {
-        iframeID: '',
-        // Path to the hosted USU Design Tools
-        path: 'https://designtools.ciditools.com/',
-        templateCourse: '1190741',
-        // OPTIONAL: Button will be hidden from view until launched using shortcut keys
-        hideButton: true,
-        // OPTIONAL: Limit tools loading by users role
-        limitByRole: false, // set to true to limit to roles in the roleArray
-        // adjust roles as needed: 'student', 'teacher', ...
-        roleArray: [],
-        // OPTIONAL: Limit tools to an array of Canvas user IDs
-        limitByUser: false, // Change to true to limit by user
-        // add users to array (Canvas user ID not SIS user ID)
-        userArray: []
-    };
-////////////////////////////////////////////////////
-// END DESIGN TOOLS CONFIG                        //
-////////////////////////////////////////////////////
+    iframeID: '',
+    // Path to the hosted USU Design Tools
+    path: 'https://designtools.ciditools.com/',
+    templateCourse: '1190741',
+    // OPTIONAL: Button will be hidden from view until launched using shortcut keys
+    hideButton: true,
+    // OPTIONAL: Limit tools loading by users role
+    limitByRole: false, // set to true to limit to roles in the roleArray
+    // adjust roles as needed: 'student', 'teacher', ...
+    roleArray: [],
+    // OPTIONAL: Limit tools to an array of Canvas user IDs
+    limitByUser: false, // Change to true to limit by user
+    // add users to array (Canvas user ID not SIS user ID)
+    userArray: []
+};
+
+window.eesyLaunchConfig = {
+    host: 'uw.eesysoft.com',
+    key: 'yrLJv5Hs'
+};
 
 (function ($) {
     'use strict';
 
+    var now = new Date().getTime();
+
+    // EesySoft
+    $.getScript('//' + window.eesyLaunchConfig.host + '/resources/js/canvas/launch.js?stmp=' + now);
+
     $(document).ready(function () {
-        var now = new Date().getTime();
         // Design Tools JS
         $.getScript(DT_variables.path + 'js/master_controls.js');
 
