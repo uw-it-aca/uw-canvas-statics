09e7114a78fc6e0a3799a4979b1e9d8193c2e5f1
Jim Laney <jlaney@uw.edu>
2020-05-18 08:00:02 -0700

    add custom js file for mathematics

---
+++ includes/department-mathematics/custom.js
@@ -0,0 +1,7 @@
+/*jslint browser: true */
+/*global jQuery */
+(function ($) {
+    "use strict";
+
+    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS_CHTML-full');
+}(jQuery));
