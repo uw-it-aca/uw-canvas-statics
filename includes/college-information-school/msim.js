8af4fe09ba354db383b5b211a0687bd8aaf37083
Jim Laney <jlaney@uw.edu>
2021-02-26 09:08:49 -0800

    updates msim statics

--- includes/college-information-school/msim.js
+++ includes/college-information-school/msim.js
@@ -3,15 +3,15 @@ var pad = document.getElementsByClassName("element_to_toggle");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var pad = this.nextElementSibling;
-        if (pad.style.display === "block") {
-            pad.style.display = "none";
-        } else {
-            pad.style.display = "block";
-        }
-    });
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var pad = this.nextElementSibling;
+    if (pad.style.display === "block") {
+      pad.style.display = "none";
+    } else {
+      pad.style.display = "block";
+    }
+  });
 }
 
 var accLive = document.getElementsByClassName("element_toggler_live");
@@ -19,35 +19,35 @@ var callIn = document.getElementsByClassName("call_in_content");
 var j;
 
 for (j = 0; j < accLive.length; j++) {
-    accLive[j].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var callIn = this.nextElementSibling;
-        if (callIn.style.display === "block") {
-            callIn.style.display = "none";
-        } else {
-            callIn.style.display = "block";
-        }
-    });
+  accLive[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var callIn = this.nextElementSibling;
+    if (callIn.style.display === "block") {
+      callIn.style.display = "none";
+    } else {
+      callIn.style.display = "block";
+    }
+  });
 }
-
 
 /////Tabs JS code
 
-var tabs = document.getElementById('icetab-container').children;
-var tabcontents = document.getElementById('icetab-content').children;
+var tabs = document.querySelectorAll(".tabs ul li");
+var tab_wraps = document.querySelectorAll(".tab_wrap");
 
-var myFunction = function() {
-	var tabchange = this.mynum;
-	for(var int=0;int<tabcontents.length;int++){
-		tabcontents[int].className = ' tabcontent-block';
-		tabs[int].className = ' icetab';
-	}
-	tabcontents[tabchange].classList.add('tab-active');
-	this.classList.add('current-tab');
-};
+tabs.forEach(function(tab, tab_index){
+  tab.addEventListener("click", function(){
+    tabs.forEach(function(tab){
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
 
-
-for(var index=0;index<tabs.length;index++){
-	tabs[index].mynum=index;
-	tabs[index].addEventListener('click', myFunction, false);
-}
+    tab_wraps.forEach(function(content, content_index){
+      if(content_index == tab_index){
+        content.style.display = "block";
+      } else {
+        content.style.display = "none";
+      }
+    });
+  });
+});
