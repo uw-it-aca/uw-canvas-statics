1a3378359a01e91ee59ec0c5ac23845b7661c0d4
Jim Laney <jlaney@uw.edu>
2023-08-25 07:10:12 -0700

    update panopto course copy message

--- includes/uw-global/content_migrations.js
+++ includes/uw-global/content_migrations.js
@@ -4,7 +4,7 @@
     'use strict';
 
     var zoom_info = '<strong>Zoom meeting access:</strong> When copying all content from a Canvas course, calendar events for Zoom meetings scheduled using the Zoom app in Canvas are also copied. To prevent students from accessing previous Zoom meeting links you can:<ul style="margin: 0 0 6px 60px"><li>Delete individual calendar events containing previous Zoom meeting links, or</li><li>Select <a href="https://community.canvaslms.com/t5/Instructor-Guide/How-do-I-select-specific-content-as-part-of-a-course-import/ta-p/1091" target="_blank">specific content to copy</a>, and exclude all calendar events from being copied</li></ul>',
-        panopto_info = '<p><strong>Access to Panopto recordings:</strong> When copying any Canvas content from one course to another, students in the new course will be granted access to the Panopto recordings in the original course. <a href="https://itconnect.uw.edu/learn/tools/panopto/remove-access-recordings/" target="_blank">You may remove access to recordings.</a></p>';
+    panopto_info = '<p><strong>Panopto archived recordings:</strong> Before copying your course in Canvas and Panopto, <a href="https://itconnect.uw.edu/tools-services-support/teaching-learning/panopto/copy-recordings/" target="_blank">make sure that you do not have any archived recordings</a> associated with that course in Panopto.  Archived Recordings will NOT copy.</p>';
 
     // Custom text for migrating course content
     $('#content').whenExists(function () {
