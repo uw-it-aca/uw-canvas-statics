43d6bce53ac36da2e32514a4c1b8b58dd0aea84b
Stephen Mats Mats <steve@matsmats.com>
2024-02-13 10:55:12 -0800

    updates to designplus CoEd

--- includes/college-education/custom.js
+++ includes/college-education/custom.js
@@ -1,25 +1,68 @@
-/*jslint browser: true */
-/*global jQuery */
-(function ($) {
-    "use strict";
+////////////////////////////////////////////////////
+// DESIGNPLUS CONFIG                            //
+////////////////////////////////////////////////////
+// Legacy
+var DT_variables = {
+        iframeID: '',
+        // Path to the hosted USU Design Tools
+        path: 'https://designtools.ciditools.com/',
+        templateCourse: '1436647',
+        // OPTIONAL: Button will be hidden from view until launched using shortcut keys
+        hideButton: true,
+    	 // OPTIONAL: Limit by course format
+	     limitByFormat: false, // Change to true to limit by format
+	     // adjust the formats as needed. Format must be set for the course and in this array for tools to load
+	     formatArray: [
+            'online',
+            'on-campus',
+            'blended'
+        ],
+        // OPTIONAL: Limit tools loading by users role
+        limitByRole: false, // set to true to limit to roles in the roleArray
+        // adjust roles as needed
+        roleArray: [
+            'student',
+            'teacher',
+            'admin'
+        ],
+        // OPTIONAL: Limit tools to an array of Canvas user IDs
+        limitByUser: false, // Change to true to limit by user
+        // add users to array (Canvas user ID not SIS user ID)
+        userArray: [
+            '1234',
+            '987654'
+        ]
+};
 
-    // If Early EDU home page, then remove the right sidebar and expand the page
-    $(".early-edu").whenExists(function () {
-        $(".ic-app-main-content")
-            .css({"flex": "", "-webkit-flex": "", "display": "block"});
-        $(".ic-Layout-contentWrapper")
-            .css({"flex": "", "-webkit-flex": "", "display": "block"});
-        $("body:not(.full-width):not(.outcomes) .ic-Layout-wrapper")
-            .css({"max-width": "none"});
-    });
-    $(".early-edu-discussion").whenExists(function () {
-        $(".discussion-reply-box").addClass("early-edu-discussion-button")
-            .children("span:first").text("Contribute to the Discussion");
-    });
-    $("#early-edu-accordion").whenExists(function () {
-        $("#early-edu-accordion").accordion({
-            collapsible: true,
-            heightStyle: "content"
-        });
-    });
-}(jQuery));
+// New
+DpPrimary = {
+    lms: 'canvas',
+    templateCourse: '1738987',
+    hideButton: true,
+    hideLti: false,
+    extendedCourse: '', // added in sub-account theme
+    sharedCourse: '', // added from localStorage
+    courseFormats: [],
+    canvasRoles: [],
+    canvasUsers: [],
+    canvasCourseIds: [],
+    plugins: [],
+    excludedModules: [],
+    includedModules: [],
+    lang: 'en',
+    defaultToLegacy: false,
+    enableVersionSwitching: true,
+    hideSwitching: false,
+};
+
+// merge with extended/shared customizations config
+DpConfig = { ...DpPrimary, ...(window.DpConfig ?? {}) };
+
+$(function () {
+    const uriPrefix = (location.href.includes('.beta.')) ? 'beta.' : '';
+    const toolsUri = (DpConfig.toolsUri) ? DpConfig.toolsUri : `https://${uriPrefix}designplus.ciditools.com/`;
+    $.getScript(`${toolsUri}js/controller.js`);
+});
+////////////////////////////////////////////////////
+// END DESIGNPLUS CONFIG                        //
+////////////////////////////////////////////////////
