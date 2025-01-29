4742fe32e0c1dfa682956a01470ffd7d05e01ef5
Stephen Mats Mats <steve@matsmats.com>
2025-01-29 12:10:50 -0800

    naive execution of replacement js and adding css

--- includes/college-medicine/custom.js
+++ includes/college-medicine/custom.js
@@ -1,55 +1,35 @@
 /*jslint browser: true, plusplus: true */
 /*jshint esversion: 11 */
-/*global jQuery */
+/*global jQuery, UWCanvas */
 
-/*EvaluationKIT START*/
-var evalkit_jshosted = document.createElement('script');
-evalkit_jshosted.setAttribute('defer', 'defer');
-evalkit_jshosted.setAttribute('type', 'text/javascript');
-evalkit_jshosted.setAttribute('src', 'https://uwsom.evaluationkit.com/canvas/js');
-document.getElementsByTagName('head')[0].appendChild(evalkit_jshosted);
-/*EvaluationKIT END*/
+// DESIGN TOOLS CONFIG
+// Copyright (C) 2016  Utah State University
 
 ////////////////////////////////////////////////////
 // DESIGNPLUS CONFIG                            //
 ////////////////////////////////////////////////////
 // Legacy
 var DT_variables = {
-        iframeID: '',
-        // Path to the hosted USU Design Tools
-        path: 'https://designtools.ciditools.com/',
-        templateCourse: '1792776',
-        // OPTIONAL: Button will be hidden from view until launched using shortcut keys
-        hideButton: true,
-    	 // OPTIONAL: Limit by course format
-	     limitByFormat: false, // Change to true to limit by format
-	     // adjust the formats as needed. Format must be set for the course and in this array for tools to load
-	     formatArray: [
-            'online',
-            'on-campus',
-            'blended'
-        ],
-        // OPTIONAL: Limit tools loading by users role
-        limitByRole: false, // set to true to limit to roles in the roleArray
-        // adjust roles as needed
-        roleArray: [
-            'student',
-            'teacher',
-            'admin'
-        ],
-        // OPTIONAL: Limit tools to an array of Canvas user IDs
-        limitByUser: false, // Change to true to limit by user
-        // add users to array (Canvas user ID not SIS user ID)
-        userArray: [
-            '1234',
-            '987654'
-        ]
+    iframeID: '',
+    // Path to the hosted USU Design Tools
+    path: 'https://designtools.ciditools.com/',
+    templateCourse: '1705122',
+    // OPTIONAL: Button will be hidden from view until launched using shortcut keys
+    hideButton: true,
+    // OPTIONAL: Limit tools loading by users role
+    limitByRole: false, // set to true to limit to roles in the roleArray
+    // adjust roles as needed: 'student', 'teacher', ...
+    roleArray: [],
+    // OPTIONAL: Limit tools to an array of Canvas user IDs
+    limitByUser: false, // Change to true to limit by user
+    // add users to array (Canvas user ID not SIS user ID)
+    userArray: []
 };
 
 // New
 DpPrimary = {
     lms: 'canvas',
-    templateCourse: '1737171',
+    templateCourse: '1737169',
     hideButton: true,
     hideLti: false,
     extendedCourse: '', // added in sub-account theme
@@ -62,9 +42,56 @@ DpPrimary = {
     excludedModules: [],
     includedModules: [],
     lang: 'en',
-    defaultToLegacy: true,
+    defaultToLegacy: false,
     enableVersionSwitching: true,
     hideSwitching: false,
+    beforeUpgrade: function($content) {
+        function cleanupStyleAttribute(element) {
+            let style = $(element).attr('style');
+            if (!style) {
+                return; // No style attribute to clean up
+            }
+            let styles = style.split(';');
+            let cleanedStyles = {};
+            for (let i = 0; i < styles.length; i++) {
+                let styleItem = styles[i].trim();
+                if (styleItem === "") {
+                   continue;
+                }
+                let parts = styleItem.split(':');
+                let property = parts[0].trim();
+                let value = parts[1].trim();
+                // Keep only the last occurrence of each property
+                cleanedStyles[property] = value;
+            }
+            // Reconstruct the style attribute
+            let cleanedStyleString = Object.keys(cleanedStyles)
+                .map(key => `${key}: ${cleanedStyles[key]}`)
+                .join('; ');
+            $(element).attr('style', cleanedStyleString).attr('data-mce-style', cleanedStyleString);
+        }
+        // Move accordion heading children styles to heading and unwrap children
+        $content.find('.kl_panels_wrapper.kl_panels_accordion .kl_panel_heading').each(function(index, heading) {
+            let headingStyle = ($(heading).attr('style')) ? $(heading).attr('style') : '';
+            if ($(this).find('*').length > 0) {
+                $(this).find('*').each(function(ind, child) {
+                    let childStyle = ($(child).attr('style'));
+                    if (childStyle) {
+                        headingStyle += childStyle;
+                    }
+                    if ($(this).prop('tagName') === 'STRONG') {
+                        $(this).attr('style', '').attr('data-mce-style', '');
+                    } else {
+                        $(this).contents().unwrap();
+                    }
+                })
+            }
+            if (headingStyle) {
+               $(heading).attr('style', headingStyle).attr('data-mce-style', headingStyle);
+               cleanupStyleAttribute(heading);
+            }
+        });
+    },
 };
 
 // merge with extended/shared customizations config
@@ -79,10 +106,27 @@ $(function () {
 // END DESIGNPLUS CONFIG                        //
 ////////////////////////////////////////////////////
 
+if (window.location.hostname === 'canvas.uw.edu') {
+    window.eesyLaunchConfig = {
+        host: 'uw.eesysoft.com',
+        key: 'yrLJv5Hs',
+        supportTab: true
+    };
+} else {
+    window.eesyLaunchConfig = {
+        host: 'uwstaging.eesysoft.com',
+        key: '5M2g7rk9',
+        supportTab: true
+    };
+}
+
 (function ($) {
     'use strict';
 
     var now = new Date().getTime();
+
+    // EesySoft
+    $.getScript('//' + window.eesyLaunchConfig.host + '/resources/js/canvas/launch.js?stmp=' + now);
 
     $(document).ready(function () {
         // Atomic Search JS
