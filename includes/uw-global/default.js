d2038508c82c30aeb418a7528dec716c07330b74
mike seibel <mikes@washington.edu>
2025-04-18 08:48:46 -0700

    hide add users nav

--- includes/uw-global/default.js
+++ includes/uw-global/default.js
@@ -23,13 +23,6 @@ var setUWCanvas = function ($) {
         unauthorized_title = "You don't have access to this content",
         unauthorized_message = '<div id="uw_unauthorized_message"><span role="alert"><h3 class="unauth-alert-head">You don\'t have access to this content.</h3><p class="unauth-alert unauth-alert-bold">Please contact the course instructor.</span><p class="unauth-alert">Describe the resource that you are trying to access and provide your UW NetID.</p></span><hr style="width:675px;"><div class="unauth-extra"><p>Common reasons content is unavailable include:</p><ul class="unauth-extra-list"><li>Content has not yet been published</li><li>You are not enrolled in the course</li><li>Link provided is incorrect</li><li>Content has been deleted</li></ul></div></div>';
 
-    // hide navigation items
-    $('head').append('<style type="text/css">' +
-                     add_users_external_ids.concat(
-                         uw_groups_external_ids.concat(
-                             course_photos_external_ids)).map(id => 'a.context_external_tool_' + id).join(',') +
-                     ' { display: none !important; }</style>');
-
     $.fn.extend({
         whenExists: function(handler) {
             var n = 0,
