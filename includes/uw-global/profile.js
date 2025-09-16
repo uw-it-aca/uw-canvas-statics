60b72deb988e2cdd005a661e6bfa96370285c34b
mike seibel <mikes@washington.edu>
2025-09-16 12:26:57 -0700

    preferred name hints update

--- includes/uw-global/profile.js
+++ includes/uw-global/profile.js
@@ -4,34 +4,8 @@
     'use strict';
 
     var add_access_token_info = '<p>Access tokens allow third-party applications to access Canvas resources on your behalf. You can generate a token to use in programmatically accessing the Canvas API.  These tokens provide the ability to interact with the API with the same permissions you have through the Canvas UI.  Because many instructors or Canvas admins have broad access to confidential student data in Canvas, these access tokens should be handled securely.  When using an API access token:</p><p><ul><li>Record the token in a secure manner.</li><li>Do not send the token through email.</li><li>Do not provide your API access token to any other individual or a vendor.</li><li>Do not use your personal access token to configure an account-level integration.</li><li>Review your access tokens annually, and expire or delete unused tokens.</li></ul></p>Learn more about <a href="https://itconnect.uw.edu/learn/tools/canvas/integration-development-data-access/" target="_blank">appropriate methods for integrating with Canvas</a>. If you are working with a vendor, contact <a href="mailto:help@uw.edu">help@uw.edu</a> for assistance.',
-        settings_hint_html = '<div class="data_description" style="padding-left: 4em; margin: -0.8em 0 0.8em 0;">Name fields are not editable in Canvas. Visit <a href="https://identity.uw.edu/profile" target="_blank">Identity.UW</a> to change your Preferred Name.</div>',
-        full_name_hint_html = '<br>Used for the Gradebook and Grades pages, Chat, People, and SIS imports.',
-        display_name_hint_html = '<br>Used for Announcements, Conferences, Conversations, and Discussions.',
-        sortable_name_hint_html = '<br>Appears in sorted lists.';
-
-    function add_settings_hint() {
-        var $settings_title = $('div#content.ic-Layout-contentMain > h1');
-
-        $settings_title.after(settings_hint_html);
-    }
-
-    function replace_full_name_hint() {
-        var $full_name_hint = $('span#hints_name');
-
-        $full_name_hint.html(full_name_hint_html);
-    }
-
-    function replace_display_name_hint() {
-        var $display_name_hint = $('span#hints_short_name');
-
-        $display_name_hint.html(display_name_hint_html);
-    }
-
-    function replace_sortable_name_hint() {
-        var $sortable_name_hint = $('span#hints_sortable_name');
-
-        $sortable_name_hint.html(sortable_name_hint_html);
-    }
+        identity_url = 'https://identity.uw.edu/profile',
+        preferred_name_hint = `cannot be edited in UW Canvas. Visit <a href="${identity_url}" target="_blank">Identity.UW</a> to change your Preferred Name.`;
 
     // Custom text for "Generate an Access Token" modal
     $(window).on('load', function () {
@@ -47,10 +21,12 @@
 
         // if full name editing is disabled, add identity.uw pointer and hints to name fields
         if ($('table.profile_table tr td span.full_name + input.edit_data').length === 0) {
-            add_settings_hint();
-            replace_full_name_hint();
-            replace_display_name_hint();
-            replace_sortable_name_hint();
+            $.each(['name', 'short_name', 'sortable_name'], function(i, label_for) {
+                var profile_label = $(`tr th label[for=user_${label_for}]`).text().replace(/:$/, ''),
+                    hint_html = `<br>${profile_label} ${preferred_name_hint}`;
+
+                $(`span#hints_${label_for}`).html(hint_html);
+            });
         }
     });
 
