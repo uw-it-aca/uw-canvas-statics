7028c1b9617638ba317bfb90c0c78953a15730a2
mike seibel <mikes@washington.edu>
2025-09-16 14:14:45 -0700

    js tidy

--- includes/uw-global/profile.js
+++ includes/uw-global/profile.js
@@ -5,8 +5,7 @@
     'use strict';
 
     var add_access_token_info = '<p>Access tokens allow third-party applications to access Canvas resources on your behalf. You can generate a token to use in programmatically accessing the Canvas API.  These tokens provide the ability to interact with the API with the same permissions you have through the Canvas UI.  Because many instructors or Canvas admins have broad access to confidential student data in Canvas, these access tokens should be handled securely.  When using an API access token:</p><p><ul><li>Record the token in a secure manner.</li><li>Do not send the token through email.</li><li>Do not provide your API access token to any other individual or a vendor.</li><li>Do not use your personal access token to configure an account-level integration.</li><li>Review your access tokens annually, and expire or delete unused tokens.</li></ul></p>Learn more about <a href="https://itconnect.uw.edu/learn/tools/canvas/integration-development-data-access/" target="_blank">appropriate methods for integrating with Canvas</a>. If you are working with a vendor, contact <a href="mailto:help@uw.edu">help@uw.edu</a> for assistance.',
-        identity_url = 'https://identity.uw.edu/profile',
-        preferred_name_hint = `cannot be edited in UW Canvas. Visit <a href="${identity_url}" target="_blank">Identity.UW</a> to change your Preferred Name.`;
+        identity_url = 'https://identity.uw.edu/profile';
 
     // Custom text for "Generate an Access Token" modal
     $(window).on('load', function () {
@@ -24,9 +23,12 @@
         if ($('table.profile_table tr td span.full_name + input.edit_data').length === 0) {
             $.each(['name', 'short_name', 'sortable_name'], function(i, label_for) {
                 var profile_label = $(`tr th label[for=user_${label_for}]`).text().replace(/:$/, ''),
-                    hint_html = `<br>${profile_label} ${preferred_name_hint}`;
+                    preferred_name_hint = `<br>${profile_label} cannot be edited in UW Canvas. `
+                      + `Visit <a href="${identity_url}" target="_blank">Identity.UW</a> `
+                      + 'to change your Preferred Name.',
+                    hint_span = `span#hints_${label_for}`;
 
-                $(`span#hints_${label_for}`).html(hint_html);
+                $(hint_span).html(preferred_name_hint);
             });
         }
     });
